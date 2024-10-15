748ffa13486e67eb3a1cc96fa39327b6ec4cb48a
LUIGHY1308 <135401032+LUIGHY1308@users.noreply.github.com>
2024-10-14 21:56:36 -0300

    Add Index js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Sirviendo los archivos estáticos generados por Vue
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Redirigir todas las solicitudes al index.html para que Vue Router se encargue de la navegación
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Puerto asignado por Heroku
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
